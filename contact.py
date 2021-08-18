244a8eef3eb066e1de0130ef9ec43200110d896a
mikhail.osinovskiy <mihaosin@yandex.ru>
2021-08-18 17:17:23 +0300

    add test_add_contact

---
+++ contact.py
@@ -0,0 +1,5 @@
+class Contact:
+    def __init__(self, firstname, lastname, mobile_number):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.mobile_number = mobile_number
