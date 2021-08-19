e86676bfddac0d1c51e76ff597dfa48fcfc238e0
mikhail.osinovskiy <mihaosin@yandex.ru>
2021-08-19 12:34:50 +0300

    optimization test readability

--- test_add_contact.py
+++ test_add_contact.py
@@ -12,20 +12,15 @@ class TestAddContact(unittest.TestCase):
 
     def test_add_contact(self):
         wd = self.wd
-        self.open_homepage(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_add_new_page(wd)
         self.add_new_contact(wd, Contact(firstname="qwerty", lastname="qwerty", mobile_number="987654321"))
-        self.return_to_home_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def return_to_home_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
     def add_new_contact(self, wd, contact):
+        wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.firstname)
@@ -36,14 +31,10 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("mobile").clear()
         wd.find_element_by_name("mobile").send_keys(contact.mobile_number)
         wd.find_element_by_name("submit").click()
-
-    def open_add_new_page(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def open_homepage(self, wd):
-        wd.get("http://localhost/addressbook/")
+        wd.find_element_by_link_text("home page").click()
 
     def login(self, wd, username, password):
+        wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
--- test_add_group.py
+++ test_add_group.py
@@ -10,20 +10,14 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_homepage(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
         self.create_group(wd, Group(name="qwerty", header="qwerty", footer="qwerty"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_empty_group(self):
         wd = self.wd
-        self.open_homepage(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def add_new_contact(self, wd):
@@ -32,10 +26,10 @@ class TestAddGroup(unittest.TestCase):
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def return_to_groups_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd, group):
+        # open group page
+        wd.find_element_by_link_text("groups").click()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -50,11 +44,12 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        #     return to group page
+        wd.find_element_by_link_text("group page").click()
 
-    def open_group_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
+        wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
@@ -62,8 +57,6 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_homepage(self, wd):
-        wd.get("http://localhost/addressbook/")
 
     def tearDown(self):
         self.wd.quit()
