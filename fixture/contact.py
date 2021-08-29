6b8f801072a994908ce8a231135374d1c844fa43
mikhail.osinovskiy <mihaosin@yandex.ru>
2021-08-29 15:29:01 +0300

    Задание №5: Реализовать двухуровневую архитектуру тестов

---
+++ fixture/contact.py
@@ -0,0 +1,21 @@
+
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app= app
+
+    def add_new(self, contact):
+        wd=self.app.wd
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_number)
+        wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("home page").click()
